e4040f3bcfe7ce58c93b4391ccc5ac3a154f5fa1
rfigueiredo27 <renan.figueiredo@globo.com>
2017-10-02 12:07:15 -0300

    Tratamento do último bloco
    Último bloco do arquivo sendo tratado.

--- src/ordenacao/Main.java
+++ src/ordenacao/Main.java
@@ -19,7 +19,7 @@ public class Main {
 		RandomAccessFile arquivo_original = new RandomAccessFile("src/ordenacao/cep.dat", "r");
 
 		//Gera o arquivo ordenado
-		RandomAccessFile arquivo_bloco_ordenado = new RandomAccessFile("src/ordenacao/cep_bloco.dat", "rw"); 
+		RandomAccessFile arquivo_bloco_ordenado = new RandomAccessFile("src/ordenacao/cep_blocos.dat", "rw"); 
 
 		//Escolhe o tamanho do bloco
 		System.out.println("Entre com o tamanho do Bloco:");
@@ -39,33 +39,71 @@ public class Main {
 		//Posição inicial
 		arquivo_original.seek(0);
 
-		System.out.println("Total contador: " + contador);
+		//Essa parte trata a parte menor que vai sobrar no final
+		int parte_inteira = 0;
+		int parte_final = 0;
+
+		parte_inteira = contador/tamanho_bloco;
+		parte_final = contador % tamanho_bloco;
+
+		boolean ultimo = false;
+
+		if(parte_final != 0)
+		{
+			ultimo = true;
+			parte_inteira = parte_inteira + 1;
+		}
 
 		List<Endereco> lista_enderecos = new ArrayList<Endereco>(tamanho_bloco + 1);
 
 
-		for(int posicao = 0; posicao < tamanho_bloco; posicao++) 
+		for(int posicao = 0; posicao < parte_inteira; posicao++) 
 		{
-			//Cria o objeto "endereco" para inserção na lista e manipulação dos atributos
-			Endereco endereco = new Endereco();
-			endereco.leEndereco(arquivo_original);
-			lista_enderecos.add(endereco);
+			//coloco o número do bloco de acordo com a posição
+			arquivo_bloco_ordenado.writeBytes((posicao+1) + "\n" );
 
-			System.out.println("Posição: " + posicao + " CEP: "  + lista_enderecos.get(posicao).getCep() + " Cidade: " + lista_enderecos.get(posicao).getCidade() + " Estado: " + lista_enderecos.get(posicao).getEstado());
+			for(int i = 0; i < tamanho_bloco; i = i++) {
+				Endereco endereco = new Endereco();
+				endereco.leEndereco(arquivo_original);
+				lista_enderecos.add(endereco);
+			}
 
-			arquivo_bloco_ordenado.writeBytes("Posição: " + posicao + " CEP: "  + lista_enderecos.get(posicao).getCep() + " Cidade: " + lista_enderecos.get(posicao).getCidade() + " Estado: " + lista_enderecos.get(posicao).getEstado());
+			//Ordenação da coleção junto com o comparator
+			Collections.sort(lista_enderecos, Endereco.COMPARA_CEP);
 
+			for(int i = 0; i < tamanho_bloco; i++) {
+				//insere os dados no novo arquivo ordenado
+				arquivo_bloco_ordenado.writeBytes(lista_enderecos.get(i).getCep() + lista_enderecos.get(i).getLogradouro()
+						+ lista_enderecos.get(i).getBairro() + lista_enderecos.get(i).getCidade()
+						+ lista_enderecos.get(i).getSigla() + "\n" ); 
+			}
+
+			lista_enderecos.clear();
 		}
 
-		//Ordenação da coleção junto com o comparator
-		Collections.sort(lista_enderecos, Endereco.COMPARA_CEP);
-
-
-		//Teste de CEP para verificar se o mesmo está ordenado nas 10 primeiras posições
-		for(int i=0; i<10; i++)
+		//Resolve o problema da última parte do arquivo
+		if(ultimo)
 		{
-			System.out.println(lista_enderecos.get(i).getCep());
+			//último_bloco
+			arquivo_bloco_ordenado.writeBytes((parte_inteira) +"\n" );
+
+			for(int i = 0; i < parte_final; i++) {
+				Endereco endereco = new Endereco();
+				endereco.leEndereco(arquivo_original);
+				lista_enderecos.add(endereco);
+			}
+
+			//ordenação da última parte
+			Collections.sort(lista_enderecos, Endereco.COMPARA_CEP);
+
+			for(int i = 0; i < parte_final; i = i++) {
+				arquivo_bloco_ordenado.writeBytes(lista_enderecos.get(i).getCep() + lista_enderecos.get(i).getLogradouro()
+						+ lista_enderecos.get(i).getBairro() + lista_enderecos.get(i).getCidade() 
+						+ lista_enderecos.get(i).getSigla()	+ "\n" );
+
+			}
 		}
+
 		arquivo_original.close();
 		arquivo_bloco_ordenado.close();
 	}
